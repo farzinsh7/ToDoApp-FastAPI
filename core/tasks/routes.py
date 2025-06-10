1a8d3f709342d78f64be16149a8e07e087b614b9
Farzin Shams <farzinnater@gmail.com>
2025-06-10 15:54:56 +0330

    get todo by id

--- core/tasks/routes.py
+++ core/tasks/routes.py
@@ -22,14 +22,28 @@ async def todos_list(db: Annotated[Session, Depends(get_db)]):
 
 
 @router.post("/todos")
-async def todo_create(request: TodoCreateSchema, db: Annotated[Session, Depends(get_db)]):
+async def create_todo(request: TodoCreateSchema, db: Annotated[Session, Depends(get_db)]):
     todo_obj = Todos(**request.model_dump())
     
     db.add(todo_obj)
     db.commit()
     db.refresh(todo_obj)
-    
+ 
     return todo_obj
+
+
+
+@router.get("/todos/{todo_id}")
+async def retrieve_todo_by_id(
+    db: Annotated[Session, Depends(get_db)],
+    todo_id: int = Path(..., gt=0), 
+    ):
+    todo_obj = db.query(Todos).filter_by(id= todo_id).first()
+    
+    if todo_obj:
+        return todo_obj
+    
+
 
 
 
