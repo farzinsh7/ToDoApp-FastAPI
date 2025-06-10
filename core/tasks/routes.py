2ad9d9bc9f3a0e478198616bccb2bcf7f2db6d22
Farzin Shams <farzinnater@gmail.com>
2025-06-10 15:47:43 +0330

    Adding todo create

--- core/tasks/routes.py
+++ core/tasks/routes.py
@@ -14,7 +14,23 @@ from tasks.models import Todos
 router = APIRouter(tags=["Todos"])
 
 
-@router.get("/todos-list", response_model=List[TodoResponseSchema])
+@router.get("/todos", response_model=List[TodoResponseSchema])
 async def todos_list(db: Annotated[Session, Depends(get_db)]):
     query = db.query(Todos).all()
     return query
+
+
+
+@router.post("/todos")
+async def todo_create(request: TodoCreateSchema, db: Annotated[Session, Depends(get_db)]):
+    todo_obj = Todos(**request.model_dump())
+    
+    db.add(todo_obj)
+    db.commit()
+    db.refresh(todo_obj)
+    
+    return todo_obj
+
+
+
+
